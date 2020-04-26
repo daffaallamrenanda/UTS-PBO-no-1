5e7a260b06c3d7afcde2f59bc803ffbe24f44028
Daffa A. Renanda <64259251+daffaallamrenanda@users.noreply.github.com>
2020-04-26 12:57:06 +0700

    Update main.java

--- main.java
+++ main.java
@@ -1,60 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package soal1;
+package soall;
 
-/**
- *
- * @author user
- */
-public class QuadraticEquation {
-    
-private double a;
-    private double b;
-    private double c;
+import java.util.Scanner;
+import soal1.QuadraticEquation;
 
-    public QuadraticEquation(double a, double b, double c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-    }
+public class Main {
 
-    public double getA() {
-        return a;
-    }
+    public static void main(String[] args) {
 
-    public void setA(double a) {
-        this.a = a;
-    }
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a, b, c: ");
+        double a = input.nextDouble();
+        double b = input.nextDouble();
+        double c = input.nextDouble();
 
-    public double getB() {
-        return b;
-    }
+        QuadraticEquation equation = new QuadraticEquation(a, b, c);
+        double discriminant = equation.getDiscriminant();
 
-    public void setB(double b) {
-        this.b = b;
-    }
+        if (discriminant > 0) {
+            System.out.println("The roots are " + equation.getRoot1()
+                    + " and " + equation.getRoot2());
+        } else if (discriminant == 0) {
+            System.out.println("The root is " + equation.getRoot1());
+        } else {
+            System.out.println("The equation has no roots");
 
-    public double getC() {
-        return c;
-    }
-
-    public void setC(double c) {
-        this.c = c;
-    }
-
-    public double getDiscriminant() {
-         return b * b - 4.0 * a * c;
-    }
-
-    public double getRoot1() {
-            return  (-b + Math.pow(getDiscriminant(), 0.5)) / (2.0 * a);
-    }
-
-    public double getRoot2() {
-            return  (-b - Math.pow(getDiscriminant(), 0.5)) / (2.0 * a);
+        }
     }
 
 }
